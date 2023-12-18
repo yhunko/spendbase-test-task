b36208bcc1b630cdc6f1e5fb01101d48b53b8e51
Yehor Hunko <57867800+yhunko@users.noreply.github.com>
2023-12-18 14:56:03 +0200

    chore: sets up tailwind with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{ts,tsx}"
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
